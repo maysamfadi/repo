f5d5095430bb571c68a247d5ebabc8e7aeb6ac3d
maysamfadi <maysamrashed92@gmail.com>
2024-08-22 17:08:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var body = document.querySelector("body");
+var changemode = document.querySelector(".changemode");
+function mode(){  
+    body.classList.toggle("light");
+}
+
+changemode.onclick = mode;
